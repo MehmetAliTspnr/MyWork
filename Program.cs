22ef77c036b5416a6c27eb77aafd1763b16d97e6
Mehmet Ali <101504173+MehmetAliTspnr@users.noreply.github.com>
2022-07-16 14:02:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aritmetik_işlem_uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float sayi1, sayi2; //Basit Aritmetik İşlem Uygulaması
+        
+
+            Console.WriteLine("İslem Yapacaginiz 1.Sayiyi Giriniz:");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            int secim; 
+
+            Console.WriteLine("İslem Yapacaginiz 2.Sayiyi Giriniz:");
+            sayi2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Yapmak istediginiz islemi seciniz:");
+
+            Console.WriteLine("1-Toplama\n2-Fark\n3-Bolum\n4-Carpim\n");
+
+            secim = Convert.ToInt32(Console.ReadLine());
+
+                switch (secim)
+                {
+                    case 1:
+                        Console.WriteLine(sayi1 + sayi2);
+                        break;
+
+                    case 2:
+                         Console.WriteLine(sayi1 - sayi2);
+                        break;
+
+                    case 3:
+                            Console.WriteLine(sayi1 / sayi2);
+                        break;
+
+                    case 4:
+                        Console.WriteLine(sayi1 * sayi2);
+                        break;
+                    default:
+                        Console.WriteLine("Seciminiz listede yok");
+                        break;
+                } 
+            Console.ReadLine();
+        }
+    }
+}
